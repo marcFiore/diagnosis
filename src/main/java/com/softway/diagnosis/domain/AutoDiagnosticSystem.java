61063690bf6f61d3c28c32595209ccd19953ab47
Marc Fiore <fiore.marc.mail@gmail.com>
2024-12-09 10:59:41 +0100

    test shouldReturnHeartProblem is green

--- src/main/java/com/softway/diagnosis/domain/AutoDiagnosticSystem.java
+++ src/main/java/com/softway/diagnosis/domain/AutoDiagnosticSystem.java
@@ -3,6 +3,9 @@ package com.softway.diagnosis.domain;
 public class AutoDiagnosticSystem implements DiagnosticSystem {
     @Override
     public String diagnose(int healthIndex) {
+        if (healthIndex % 3 == 0) {
+            return "Cardiologie";
+        }
         return null;
     }
 }
