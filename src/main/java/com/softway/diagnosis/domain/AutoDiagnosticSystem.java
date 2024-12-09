3b0f48188d1f584889a930585514beee77cf17c8
Marc Fiore <fiore.marc.mail@gmail.com>
2024-12-09 11:04:02 +0100

    pass both problem test case to green

--- src/main/java/com/softway/diagnosis/domain/AutoDiagnosticSystem.java
+++ src/main/java/com/softway/diagnosis/domain/AutoDiagnosticSystem.java
@@ -3,6 +3,11 @@ package com.softway.diagnosis.domain;
 public class AutoDiagnosticSystem implements DiagnosticSystem {
     @Override
     public String diagnose(int healthIndex) {
+
+        if (healthIndex % 3 == 0 && healthIndex % 5 == 0) {
+            return "Cardiologie, Traumatologie";
+        }
+
         if (healthIndex % 3 == 0) {
             return "Cardiologie";
         }
